9165d5229404f586b16da64e9f720a1391a2f2cb
Deusagnis <deusagnis@gmail.com>
2021-09-10 13:09:07 +0300

    Fix home root.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
