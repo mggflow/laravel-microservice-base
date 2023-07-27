b3288dd51fd7599d99603b17b8d4caec1e4cf997
Deusagnis <deusagnis@gmail.com>
2023-07-27 20:28:48 +0300

    Fix pgsql database encoding.

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf-8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
